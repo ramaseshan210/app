d5ae6e4e4398c827d2d7489b29b1b04b57325332
ramaseshan210 <ramannp210@gmail.com>
2020-12-11 15:54:07 +0530

    initialize commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import { Switch } from '@material-ui/core';
+import Forms from "./Forms"
+import Header from "./Header"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Use from './Use';
+import {BrowserRouter as Router, Route } from "react-router-dom"; 
+
+
+class App extends Component {
+ 
+   state= {
+      myString:"Hello",
+      myStringOne:"hey ram",
+      string:"hai",
+      label:"press me",
+      counter: 0
+   };
+
+
+
+   onCounter = () => {
+
+      this.setState({
+            counter:this.state.counter + 1
+      });
+
+
+   };                                                       
+
+
+   onCount = () =>{
+
+      if(this.state.string === "hai")
+      {
+         
+      this.setState({   
+         string:"django"});
+      }
+      else if(this.state.string === "django")
+      {
+         this.setState({
+               string:"hai"});
+         
+      }
+};
+
+   onHandle = () => {
+      if(this.state.myStringOne === "hey ram")
+      { 
+            this.setState({
+               myStringOne:"rajavu"});
+      }
+      else if(this.state.myStringOne === "rajavu")
+      {
+      this.setState({
+         myStringOne:"hey ram"
+      });
+   };
+
+  
+
 }
+
+   render(){
+      return (
+         <div>
+
+    
+               <Router>
+                  <Header/>
+                  <Route path="/"component={Forms}/>
+                   <Route path="./about"component={Use}/>
+               </Router>
+            
+
+
+
+
+
+        
+           {/* <Useforms/>*/}
+           {/*<Forms />*/}
+           {/* <Header/> *}
+            
+            <h1>hello</h1>
+            <h1>{this.state.myStringOne}</h1>
+            <button onClick={this.onHandle}>{this.state.label}</button>
+            <button onClick={this.onCount}>count</button>
+            <button onClick={this.onCounter}>counter</button>
+          {/* <Todo myStringOne={this.state.myStringOne}/>
+           <Todo myStringOne={this.state.string}/>
+           <Todo myStringOne={this.state.counter}/>
+           */}
+           
+         </div>
+      );
+   }
+}  
+
 
 export default App;
